0a41ffdbd5bf5db415d9b6678f460bad51208afc
Luis Fernando Varón Acosta <luih.pm.14@gmail.com>
2024-08-29 16:07:44 -0500

    Built secure password gen but with some details where PEP8 leaks

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-
-
+from password_gen import *
 
 if __name__ == "__main__":
-    pass
+    length = get_password_length()
+    password = password_build(length)
+    print(password_quality_assurance(password))
+    test = "AAAAAAAAAAAAAAA"
+    test = password_quality_assurance(test)
+    print(f"Test result {test}")
--- password_gen.py
+++ password_gen.py
@@ -1,29 +1,98 @@
 # Secure Password Generator
-# -------------------------------------------------------------------------------------------
+# ------------------------------------------------
 # * Use Variables, Strings, Loops, Functions. 
-# 1. A password should be at least 12 characters long (idealy 16 characters or more)
-# 2. A password should include a combination of letters (both uppercase and lowercase),
-# numbers, and characters
-# 3. A password shouldn't include any of your personal information.
-# 4. A password shouldn't contain any consecutive letters or numbers (i.e. ABCD, 1234, etc.)
-# 5. A password shouldn't be the word 'password or the same letter or number repeated.'
+# 1. A password should be at least 12 characters 
+#    long (ideally 16 characters or more).
+# 2. A password should include a combination of 
+#    letters (both uppercase and lowercase), numbers, 
+#    and characters.
+# 3. A password shouldn't include any of your 
+#    personal information.
+# 4. A password shouldn't contain any consecutive 
+#    letters or numbers (i.e., ABCD, 1234, etc.)
+# 5. A password shouldn't be the word 'password' 
+#    or the same letter or number repeated.
 
 from random import choice
 
-uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-lowercase = "abcdefghijklmnopqrstuvwxyz"
-numbers = "1234567890"
-symbols = "!#$%&/()=?¡¨*[];:_,.-+'¿<>|°¬@̣"
+UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+LOWERCASE = "abcdefghijklmnopqrstuvwxyz"
+NUMBERS = "1234567890"
+SYMBOLS = "!#$%&/()=?¡¨*[];:_,.-+'¿<>|°¬@̣"
 
-character_choice = [uppercase, lowercase, numbers, symbols]
+CHARACTER_CHOICE = [UPPERCASE, LOWERCASE, NUMBERS, SYMBOLS]
 
-password_legnth = int(input("Please enter the legnth of the password (between 12 and 16): "))
+def get_password_length() -> int:
+    """
+    This function asks the user for the desired length
+    they want the password to be.
+    
+    Returns:
+        int: password length 
+    """
+    
+    while True:
+        try:
+            length = int(input(
+                "Please enter the length of the password "
+                "(between 12 and 16): "
+                ))
+        except ValueError as e:
+            print(f"Error, invalid input: {e}")
+        else:
+            if 12 <= length <= 16:
+                break
+        
+    return length
 
-password = ""
+def password_build(length: int) -> str:
+    """
+    This function builds the password without
+    quality assurance using the random module and
+    a set of characters.
+    
+    Args:
+        length (int): password length
 
-for i in range(0, password_legnth):
-    character_set = choice(character_choice)
-    char = choice(character_set)
-    password += str(char)
+    Returns:
+        str: password without quality assurance
+    """
+    
+    password = ""
+    
+    for _ in range(length):
+        character_set = choice(CHARACTER_CHOICE)
+        char = choice(character_set)
+        password += str(char)
+    
+    return password
 
-print(password)
+def password_quality_assurance(password: str) -> str:
+    """
+    This function checks if the password 
+    generated has consecutive repeated chars.
+    If it does, it replaces them with a new char.
+    Also, it can be used to enhance existing 
+    passwords.
+
+    Args:
+        password (str): Password previously 
+        generated or password to enhance
+    Returns:
+        str: Password quality assurance completed
+    """
+
+    def pick_new_char() -> str:
+        character_set = choice(CHARACTER_CHOICE)
+        char = choice(character_set)
+        return char
+    
+    password_list = list(password)
+    
+    for i in range(1, len(password_list)):
+        if password_list[i] == password_list[i-1]:
+            password_list[i] = pick_new_char()
+            print(f"Found repeated consecutive character"
+                  f" '{password_list[i-1]}', formatting")
+        
+    return ''.join(password_list)
